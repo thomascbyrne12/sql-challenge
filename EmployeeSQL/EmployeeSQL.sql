8f7cdcd3a75c7e04ca261089f7392b62c58cca44
thomascbyrne12 <47927482+thomascbyrne12@users.noreply.github.com>
2019-11-17 11:48:00 -0500

    Update EmployeeSQL.sql

--- EmployeeSQL/EmployeeSQL.sql
+++ EmployeeSQL/EmployeeSQL.sql
@@ -6,36 +6,44 @@ CREATE TABlE departments (
 CREATE TABLE dept_emp (
 	emp_no VARCHAR(20),
 	dept_no VARCHAR(20),
-	from_date VARCHAR(20),
-	to_date VARCHAR(20)
+	from_date DATE,
+	to_date DATE
 );
 
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(20),
 	emp_no VARCHAR(20),
-	from_date VARCHAR(20),
-	to_date VARCHAR(20)
+	from_date DATE,
+	to_date DATE
 );
 
 CREATE TABLE employees (
 	emp_no VARCHAR(20),
-	birth_date VARCHAR(20),
+	birth_date DATE,
 	first_name VARCHAR(20),
 	last_name VARCHAR(20),
 	gender VARCHAR(20),
-	hire_date VARCHAR(20)
+	hire_date DATE
 );
 
 CREATE TABLE salaries (
 	emp_no VARCHAR(20),
 	salary INT,
-	from_date VARCHAR(20),
-	to_date VARCHAR(20)
+	from_date DATE,
+	to_date DATE
 );
 
 CREATE TABLE titles (
 	emp_no VARCHAR(20),
 	title VARCHAR(30),
-	from_date VARCHAR(20),
-	to_date VARCHAR(20)
+	from_date DATE,
+	to_date DATE
 );
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, salaries.salary
+FROM salaries
+INNER JOIN employees ON
+employees.emp_no = salaries.emp_no;
+
+SELECT * FROM employees
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
