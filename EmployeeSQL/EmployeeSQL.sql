69e790dcbec2d41007e3a1f4af90d24e5e30ca0e
thomascbyrne12 <47927482+thomascbyrne12@users.noreply.github.com>
2019-12-14 20:46:25 -0500

    Update EmployeeSQL.sql

--- EmployeeSQL/EmployeeSQL.sql
+++ EmployeeSQL/EmployeeSQL.sql
@@ -1,24 +1,10 @@
 CREATE TABlE departments (
-	dept_no VARCHAR(20),
-	dept_name VARCHAR(20)
-);
-
-CREATE TABLE dept_emp (
-	emp_no VARCHAR(20),
-	dept_no VARCHAR(20),
-	from_date DATE,
-	to_date DATE
-);
-
-CREATE TABLE dept_manager (
-	dept_no VARCHAR(20),
-	emp_no VARCHAR(20),
-	from_date DATE,
-	to_date DATE
+	dept_no VARCHAR(20) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE employees (
-	emp_no VARCHAR(20),
+	emp_no VARCHAR(20) PRIMARY KEY NOT NULL,
 	birth_date DATE,
 	first_name VARCHAR(20),
 	last_name VARCHAR(20),
@@ -26,15 +12,35 @@ CREATE TABLE employees (
 	hire_date DATE
 );
 
-CREATE TABLE salaries (
+CREATE TABLE dept_emp (
+	emp_no VARCHAR(20) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	dept_no VARCHAR(20),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+	from_date DATE,
+	to_date DATE
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(20) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 	emp_no VARCHAR(20),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	from_date DATE,
+	to_date DATE
+);
+
+CREATE TABLE salaries (
+	emp_no VARCHAR(20) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	salary INT,
 	from_date DATE,
 	to_date DATE
 );
 
 CREATE TABLE titles (
-	emp_no VARCHAR(20),
+	emp_no VARCHAR(20) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	title VARCHAR(30),
 	from_date DATE,
 	to_date DATE
