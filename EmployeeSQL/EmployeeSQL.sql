372b5e01264fe8cc0af94978bebd9a17bd7a81d7
Thomas Byrne <thomascbyrne563@gmail.com>
2021-06-22 13:27:09 -0400

    Reformating

--- EmployeeSQL/EmployeeSQL.sql
+++ EmployeeSQL/EmployeeSQL.sql
@@ -3,6 +3,7 @@ CREATE TABlE departments (
 	dept_name VARCHAR(20) NOT NULL
 );
 
+
 CREATE TABLE employees (
 	emp_no VARCHAR(20) PRIMARY KEY NOT NULL,
 	birth_date DATE,
@@ -11,6 +12,7 @@ CREATE TABLE employees (
 	gender VARCHAR(20),
 	hire_date DATE
 );
+
 
 CREATE TABLE dept_emp (
 	emp_no VARCHAR(20) NOT NULL,
@@ -21,6 +23,7 @@ CREATE TABLE dept_emp (
 	to_date DATE
 );
 
+
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(20) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
@@ -30,6 +33,7 @@ CREATE TABLE dept_manager (
 	to_date DATE
 );
 
+
 CREATE TABLE salaries (
 	emp_no VARCHAR(20) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
@@ -37,6 +41,7 @@ CREATE TABLE salaries (
 	from_date DATE,
 	to_date DATE
 );
+
 
 CREATE TABLE titles (
 	emp_no VARCHAR(20) NOT NULL,
@@ -46,54 +51,97 @@ CREATE TABLE titles (
 	to_date DATE
 );
 
-SELECT employees.emp_no, employees.last_name, employees.first_name, salaries.salary
-FROM salaries
+
+SELECT 
+	employees.emp_no, employees.last_name, employees.first_name, salaries.salary
+FROM 
+	salaries
 INNER JOIN employees ON
-employees.emp_no = salaries.emp_no;
+	employees.emp_no = salaries.emp_no;
 
-SELECT * FROM employees
-WHERE EXTRACT(YEAR FROM hire_date) = 1986;
 
-SELECT dept_manager.dept_no, departments.dept_name,
+SELECT 
+	* 
+FROM 
+	employees
+WHERE 
+	EXTRACT(YEAR FROM hire_date) = 1986;
+
+
+SELECT 
+	dept_manager.dept_no, departments.dept_name,
 	dept_manager.emp_no, employees.first_name, employees.last_name,
 	dept_manager.from_date, dept_manager.to_date
-FROM dept_manager
-INNER JOIN departments ON
-dept_manager.dept_no = departments.dept_no
-INNER JOIN employees ON
-dept_manager.emp_no = employees.emp_no;
+FROM 
+	dept_manager
+INNER JOIN 
+	departments ON
+	dept_manager.dept_no = departments.dept_no
+INNER JOIN 
+	employees ON
+	dept_manager.emp_no = employees.emp_no;
 
-SELECT dept_emp.dept_no, employees.first_name,
+
+SELECT 
+	dept_emp.dept_no, employees.first_name,
   	employees.last_name, departments.dept_name
-FROM employees
-INNER JOIN dept_emp ON
-dept_emp.emp_no = employees.emp_no
-INNER JOIN departments ON
-dept_emp.dept_no = departments.dept_name;
+FROM 
+	employees
+INNER JOIN 
+	dept_emp ON
+	dept_emp.emp_no = employees.emp_no
+INNER JOIN 
+	departments ON
+	dept_emp.dept_no = departments.dept_name;
 
-SELECT * FROM employees
-WHERE first_name = 'Hercules' AND last_name LIKE 'B_';
 
-SELECT employees.emp_no, employees.first_name,
+SELECT 
+	* 
+FROM 
+	employees
+WHERE 
+	first_name = 'Hercules' 
+AND 
+	last_name LIKE 'B_';
+
+
+SELECT 
+	employees.emp_no, employees.first_name,
 	employees.last_name, departments.dept_name
-FROM employees
-INNER JOIN dept_emp ON
-dept_emp.emp_no = employees.emp_no
-INNER JOIN departments ON
-dept_emp.dept_no = departments.dept_name
-WHERE departments.dept_name = 'Sales';
+FROM 
+	employees
+INNER JOIN 
+	dept_emp ON
+	dept_emp.emp_no = employees.emp_no
+INNER JOIN 
+	departments ON
+	dept_emp.dept_no = departments.dept_name
+WHERE 
+	departments.dept_name = 'Sales';
 
-SELECT employees.emp_no, employees.first_name,
+
+SELECT 
+	employees.emp_no, employees.first_name,
   	employees.last_name, departments.dept_name
-FROM employees
-INNER JOIN dept_emp ON
-dept_emp.emp_no = employees.emp_no
-INNER JOIN departments ON
-dept_emp.dept_no = departments.dept_name
-WHERE departments.dept_name = 'Sales'
-AND departments.dept_name = 'Development';
+FROM 
+	employees
+INNER JOIN 
+	dept_emp ON
+	dept_emp.emp_no = employees.emp_no
+INNER JOIN 
+	departments ON
+	dept_emp.dept_no = departments.dept_name
+WHERE 
+	departments.dept_name = 'Sales'
+AND 
+	departments.dept_name = 'Development';
 
-SELECT employees.last_name, count(*)
-FROM employees
-GROUP BY employees.last_name
-ORDER BY count(*) DESC;
+
+SELECT 
+	employees.last_name, count(*)
+FROM 
+	employees
+GROUP BY 
+	employees.last_name
+ORDER BY 
+	count(*) DESC;
